346300ae2a7c44521d77429bdeff6ccca9114a74
Dewminikw <dkweerabahu@gmail.com>
2021-04-21 02:42:34 +0530

    retrieve donation

--- GadgetBadget_PAF_Project/src/com/FundingService.java
+++ GadgetBadget_PAF_Project/src/com/FundingService.java
@@ -1,5 +1,80 @@
 package com;
 
+import model.Funders;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+
 public class FundingService {
 
+	Funders fundingObj = new Funders(); 
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	
+	public String readItems() 
+	 { 
+		return fundingObj.readDonations();
+	 }
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public String insertDonations(@FormParam("name") String name, @FormParam("email") String email, @FormParam("amount") String amount, @FormParam("cardNo") String cardNo, @FormParam("cvv") String cvv, @FormParam("comments") String comments)
+	{
+		String output = fundingObj.insertDonations(name, email, amount, cardNo, cvv, comments);
+		return output;
+	}
+
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public String updateDonation(String donationData) 
+	{
+		// Convert the input string to a JSON object
+		JsonObject fundObject = new JsonParser().parse(donationData).getAsJsonObject();
+
+		// Read the values from the JSON object
+		String innovID = fundObject.get("innovID").getAsString();
+		String name = fundObject.get("name").getAsString();
+		String email = fundObject.get("email").getAsString();
+		String amount = fundObject.get("amount").getAsString();
+		String cardNo = fundObject.get("cardNo").getAsString();
+		String cvv = fundObject.get("cvv").getAsString();
+		String comments = fundObject.get("comments").getAsString();
+		String output = fundingObj.updateDonation(innovID, name, email, amount, cardNo, cvv, comments);
+		return output;
+	}
+
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteDonation(String donationData) 
+	{
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(donationData, "", Parser.xmlParser());
+
+		// Read the value from the element <innovationID>
+		String innovID = doc.select("innovID").text();
+		String output = fundingObj.deleteDonation(innovID);
+		return output;
+	}
+	
 }
--- GadgetBadget_PAF_Project/src/model/Funders.java
+++ GadgetBadget_PAF_Project/src/model/Funders.java
@@ -1,6 +1,10 @@
 package model;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Funders {
 
@@ -11,8 +15,8 @@ public class Funders {
 			 { 
 				 Class.forName("com.mysql.jdbc.Driver"); 
 			 
-				 //Provide the correct details: DBServer/DBName, username, password 
-				 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", ""); 
+				 //Provide the correct details: DBServer/DBName, userName, password 
+				 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_db?useTimezone=true&serverTimezone=UTC", "root", "mysql80");
 			 } 
 			 catch (Exception e) 
 			 {
@@ -22,7 +26,7 @@ public class Funders {
 	 } 
 	
 	
-	public String insertDonation(String name, String email, String phone, String streetAddress, String city, String amount) 
+	public String insertDonations(String name, String email, String amount, String cardNo, String cvv, String comments) 
 	 { 
 		String output = ""; 
 		
@@ -34,16 +38,16 @@ public class Funders {
 					 return "Error while connecting to the database for inserting."; 
 					 } 
 				 // create a prepared statement
-				 String query = " insert into donation (`itemID`,`name`,`email`,`phone`,`streetAddress`, 'city', 'amount')" + " values (?, ?, ?, ?, ?, ?)"; 
+				 String query = " insert into donation (`innovID`,`name`,`email`,`amount`,`cardNo`,`cvv`,`comments`)" + " values (?, ?, ?, ?, ?, ?)"; 
 				 PreparedStatement preparedStmt = con.prepareStatement(query); 
 				 // binding values
 				 preparedStmt.setInt(1, 0); 
 				 preparedStmt.setString(2, name); 
 				 preparedStmt.setString(3, email); 
-				 preparedStmt.setString(4, phone);
-				 preparedStmt.setString(5, streetAddress);
-				 preparedStmt.setString(6, city);
-				 preparedStmt.setDouble(7, Double.parseDouble(amount)); 
+				 preparedStmt.setInt(4, Integer.parseInt(amount)); 
+				 preparedStmt.setInt(5, Integer.parseInt(cardNo));
+				 preparedStmt.setInt(6, Integer.parseInt(cvv));
+				 preparedStmt.setString(7, comments);
 			
 				 
 				 preparedStmt.execute(); 
@@ -52,12 +56,14 @@ public class Funders {
 			 } 
 			 catch (Exception e) 
 			 { 
-				 output = "Error while inserting the item."; 
+				 output = "Error while inserting the donation."; 
 				 System.err.println(e.getMessage()); 
 			 } 
 		return output; 
 	 } 
-	public String readItems() 
+	
+	
+	public String readDonations() 
 	 { 
 		String output = ""; 
 			 try
@@ -67,43 +73,45 @@ public class Funders {
 					 {
 					 return "Error while connecting to the database for reading."; 
 					 } 
-				 // Prepare the html table to be displayed
+				
+					 // Prepare the html table to be displayed
 				 output = "<table border='1'><tr>" + 
-						 "<th>Name</th><th>Email</th>" +
-						 "<th>Phone Number</th>" + 
-						 "<th>Street Address</th>" +
-						 "<th>City</th>" +
+						 "<th>Name</th>" + 
+						 "<th>Email</th>" + 
 						 "<th>Donation Amount</th>" +
+						 "<th>Card Number</th>" + 
+						 "<th>CVV</th>" + 
+						 "<th>Comments</th>" + 
 						 "<th>Update</th><th>Remove</th></tr>"; 
 				 
 				 String query = "select * from donation"; 
-				 Statement stmt = con.createStatement(); 
-				 ResultSet rs = stmt.executeQuery(query); 
+				 Statement stmt = (Statement)con.createStatement(); 
+				 ResultSet rs = ((java.sql.Statement)stmt).executeQuery(query); 
 				 // iterate through the rows in the result set
 					 
 				 while (rs.next()) 
 					 { 
-					 String itemID = Integer.toString(rs.getInt("itemID")); 
+					 String innovID = Integer.toString(rs.getInt("innovID")); 
 					 String name = rs.getString("name"); 
 					 String email = rs.getString("email"); 
-					 String phone = rs.getString("phone"); 
-					 String streetAddress = rs.getString("streetAddress"); 
-					 String city = rs.getString("city"); 
-					 String amount = Double.toString(rs.getDouble("amount")); 
+					 String amount = Integer.toString(rs.getInt("amount")); 
+					 String cardNo = Integer.toString(rs.getInt("cardNo")); 
+					 String cvv = Integer.toString(rs.getInt("cvv")); 
+					 String comments = rs.getString("comments");
 					 
 					 // Add into the html table
 					 output += "<tr><td>" + name + "</td>"; 
 					 output += "<td>" + email + "</td>";
-					 output += "<td>" + phone + "</td>";
-					 output += "<td>" + streetAddress + "</td>"; 
-					 output += "<td>" + city + "</td>"; 
 					 output += "<td>" + amount + "</td>"; 
+					 output += "<tr><td>" + cardNo + "</td>"; 
+					 output += "<tr><td>" + cvv + "</td>";
+					 output += "<tr><td>" + comments + "</td>"; 
 				
 					 // buttons
 					 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
-							 	+ "<td><form method='post' action='items.jsp'>"
+							 	+ "<td><form method='post' action='donation.jsp'>"
 							    + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
-							    + "<input name='itemID' type='hidden' value='" + itemID + "'>" + "</form></td></tr>"; 
+							    + "<input name='itemID' type='hidden' value='" + innovID + "'>" + "</form></td></tr>"; 
 					 } 
 				 con.close(); 
 				 // Complete the html table
@@ -112,10 +120,83 @@ public class Funders {
 				 
 			 catch (Exception e) 
 			 { 
-				 output = "Error while reading the items."; 
+				 output = "Error while reading the donation."; 
 				 System.err.println(e.getMessage()); 
 				 } 
 			 return output; 
 	 } 
+	
+	
+	public String updateDonation(String ID, String name, String email, String amount, String cardNo, String cvv, String comments) 
+	
+	{ 
+		 String output = ""; 
+				 try
+				 { 
+					 Connection con = connect(); 
+						 if (con == null) 
+						 {
+							 return "Error while connecting to the database for updating.";
+							 
+						 } 
+						 // create a prepared statement
+						 String query = "UPDATE donation SET name=?,email=?,amount=?,cardNo=?,cvv=?,comments=? WHERE innovID=?"; 
+						 PreparedStatement preparedStmt = con.prepareStatement(query); 
+						 // binding values
+						 preparedStmt.setString(1, name); 
+						 preparedStmt.setString(2, email);
+						 preparedStmt.setString(3, amount); 
+						 preparedStmt.setString(4, cardNo);
+						 preparedStmt.setString(5, cvv); 
+						 preparedStmt.setString(6, comments); 
+						 preparedStmt.setInt(7, Integer.parseInt(ID)); 
+						
+						 // execute the statement
+						 preparedStmt.execute(); 
+						 con.close(); 
+						 output = "Updated successfully"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "Error while updating the donation."; 
+					 System.err.println(e.getMessage()); 
+				 } 
+			 return output; 
+			 } 
+		
+	
+	public String deleteDonation(String innovID) 
+		 { 
+			String output = ""; 
+				 
+			try
+				 { 
+					 Connection con = connect(); 
+						 if (con == null) 
+						 {
+							 return "Error while connecting to the database for deleting."; 
+							 
+						 } 
+						
+						 // create a prepared statement
+						 String query = "delete from donation where innovID=?"; 
+						 PreparedStatement preparedStmt = con.prepareStatement(query); 
+						
+						 // binding values
+						 preparedStmt.setInt(1, Integer.parseInt(innovID)); 
+						
+						 // execute the statement
+						 preparedStmt.execute(); 
+						 con.close(); 
+						 output = "Deleted successfully"; 
+				 } 
+		 
+			catch (Exception e) 
+			 { 
+					output = "Error while deleting the donation."; 
+					System.err.println(e.getMessage()); 
+			 } 
+		 return output; 
+		 } 
 	
 }
